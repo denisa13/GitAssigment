be25afa52a951620fbbc3ae91efd2a8173981012
Denisa <denisaelena1331@gmail.com>
2020-03-22 19:53:47 +0200

    Created class Stoc.java

---
+++ src/ro/ase/csie/gr1084/Tema2Git/Stoc.java
@@ -0,0 +1,15 @@
+package ro.ase.csie.gr1084.Tema2Git;
+
+public class Stoc  {
+	int nrIntrareInStoc;
+	int codProdus;
+	float cantitate;
+
+	public Stoc(int nrIntrareInStoc, int codProdus, float cantitate) {
+		super();
+		this.nrIntrareInStoc = nrIntrareInStoc;
+		this.codProdus = codProdus;
+		this.cantitate = cantitate;
+	}
+
+}
