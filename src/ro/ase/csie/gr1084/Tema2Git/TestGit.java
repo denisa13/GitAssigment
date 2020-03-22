a8860c082cf54d31d5188cd2d8b18d3d36199150
Denisa <denisaelena1331@gmail.com>
2020-03-22 19:49:55 +0200

    Inital commit-Added the TestGit class

---
+++ src/ro/ase/csie/gr1084/Tema2Git/TestGit.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.gr1084.Tema2Git;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+
+		String testMessage = "Hello Git !" + " Denumirea proiectului de licență este: "
+				+ "Aplicatie software pentru rezolvarea problemelor de business";
+		System.out.print(testMessage);
+		
+	}
+}
